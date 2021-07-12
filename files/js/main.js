e94d8ff2ea6be9a559d33c8390aa41b71f905a41
Rafiul Awal Rafi <44731271+rafi0x@users.noreply.github.com>
2021-07-13 01:51:11 +0600

    Update main.js

--- files/js/main.js
+++ files/js/main.js
@@ -1,28 +1,10 @@
 (function($) {
     /*-------------------------------------
-<<<<<<< HEAD
-	Demo custom JavaScript
-=======
 	Demo custom JavaScripts
->>>>>>> 37bf6b25664f328d9fc2ffb2dcd05de621d9d6b1
 	-------------------------------------*/
-    $("[data-bg-image]").each(function() {
-        const img = $(this).data("bg-image");
-        $(this).css({
-            backgroundImage: `url(${img})`,
-        });
-    });
-
     /*-------------------------------------
       
       -------------------------------------*/
-    window.onload = addNewClass();
-
-    function addNewClass() {
-        $(".fxt-template-animation")
-            .imagesLoaded()
-            .done((instance) => {
-                $(".fxt-template-animation").addClass("loaded");
-            });
+	
     }
 })(jQuery);
