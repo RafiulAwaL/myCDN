d1cc2a9b678ed60cc7b5e48055d051a744719699
Rafiul Awal Rafi <44731271+rafi0x@users.noreply.github.com>
2021-07-13 16:10:31 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+/*
+ * title: Local CDN
+ * author: rafi0x
+ * description: practice project
+ * date: 07-13-2021
+ */
+
+// dependencies
 const express = require('express');
 const morgan = require('morgan');
 const routez = require('./cdn/cdnRoute');
@@ -5,7 +13,9 @@ const routez = require('./cdn/cdnRoute');
 // main app
 const app = express();
 
+// middleware to beautify console response
 app.use(morgan('dev'));
+
 
 // main index route
 app.get('/', (req, res) => {
@@ -16,5 +26,4 @@ app.get('/', (req, res) => {
 app.use('/cdn', routez);
 
 // server
-app.listen(4000, () => console.log('cdn server on 4000'));
-// @TODO: host your all content in "files" dir on root otherwise make change in controller.
+app.listen(4040, () => console.log('cdn server on 4040'));
