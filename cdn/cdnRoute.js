f002c91c99d7dc0703dd8da92bc0fc70986ad809
Rafiul Awal Rafi <44731271+rafi0x@users.noreply.github.com>
2021-07-13 16:10:07 +0600

    Update cdnRoute.js

--- cdn/cdnRoute.js
+++ cdn/cdnRoute.js
@@ -1,14 +1,13 @@
 const route = require('express').Router();
 const { getContentFile } = require('./cdnController')
-const fs = require('fs');
 
 // index route
 route.get('/', (req, res) => {
     res.send('try /css?file=filename || /js?file=filename || /image?file=filename');
 })
 
-// getting file by url query. Ex: cdn/css?file=style.css
-route.get('/:file', getContentFile)
+// getting file by param. Ex: cdn/dirName/fileName
+route.get('^/:dir/:file?', getContentFile)
 
 
 module.exports = route;
