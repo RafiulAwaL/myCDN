d690d7a78381b5dcda7f46546a6851796257930e
Rafiul Awal Rafi <44731271+rafi0x@users.noreply.github.com>
2021-07-13 13:53:55 +0600

    Update cdnController.js

--- cdn/cdnController.js
+++ cdn/cdnController.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
+const path = require('path');
+
+// getting root dir
+const dir = path.resolve('./')
 
 exports.getContentFile = (req, res) => {
     const fileName = req.query.file;
-    fs.readdir(`${__dirname}/files${req.path}/`, (err, files) => {
+    fs.readdir(`${dir}/files${req.path}/`, (err, files) => {
         // if any err happen or user give wrong dir name that dosent exist.
         if (err || !files) {
             console.log(`${err}\nwrong cdn path. please try again`);
@@ -14,7 +18,7 @@ exports.getContentFile = (req, res) => {
         }
         // if get input with query, send the file
         else {
-            res.sendFile(`${__dirname}/files${req.path}/${fileName}`);
+            res.sendFile(`${dir}/files${req.path}/${fileName}`);
         }
     })
 }
